8b995532545876f0d127aab6e7c53e7232b834ea
ThomasCode92 <thomascode92@hotmail.com>
2023-10-17 20:37:33 +0200

    Contributing to the Campaign

--- backend/contracts/Campaign.sol
+++ backend/contracts/Campaign.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.21;
 contract Campaign {
     address public manager;
     uint256 public minimumContribution;
+    address[] public approvers;
 
     constructor(uint256 minimum) {
         manager = msg.sender;
         minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minimumContribution);
+        approvers.push(msg.sender);
     }
 }

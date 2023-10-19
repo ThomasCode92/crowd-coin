9a873768b7a6b18fe58325be4d5c9b2c9c7ef03e
ThomasCode92 <thomascode92@hotmail.com>
2023-10-19 19:53:41 +0200

    The Wrong Voting System

--- backend/contracts/Campaign.sol
+++ backend/contracts/Campaign.sol
@@ -29,6 +29,22 @@ contract Campaign {
         approvers.push(msg.sender);
     }
 
+    function approveRequest(Request memory request) public {
+        // Make sure person calling this function has donated
+        bool isApprover = false;
+        for (uint i = 0; i < approvers.length; i++) {
+            if (approvers[i] == msg.sender) {
+                isApprover = true;
+            }
+        }
+        require(isApprover);
+
+        // Make sure person calling this function hasn't voted before
+        for (uint i = 0; i < request.approvers.length; i++) {
+            require(approvers != msg.sender);
+        }
+    }
+
     function createRequest(
         string memory description,
         uint256 value,

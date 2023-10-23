5c4cc4cf4dbe85f19989cb127044b895089e80cb
ThomasCode92 <thomascode92@hotmail.com>
2023-10-23 08:03:25 +0200

    Another Small Refactor

--- backend/contracts/Campaign.sol
+++ backend/contracts/Campaign.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.8.21;
 contract Campaign {
     address public manager;
     uint256 public minimumContribution;
-    uint private currentRequestIndex = 0;
     mapping(address => bool) public approvers;
-    mapping(uint => Request) public requests;
+    Request[] public requests;
 
     struct Request {
         string description;
@@ -35,11 +34,8 @@ contract Campaign {
     function approveRequest(uint256 requestIndex) public {
         Request storage request = requests[requestIndex];
 
-        // Make sure person calling this function has donated
         require(approvers[msg.sender]);
-
-        // Make sure person calling this function hasn't voted before
-        require(request.approvals[msg.sender]);
+        require(!request.approvals[msg.sender]);
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
@@ -49,15 +45,13 @@ contract Campaign {
         string memory description,
         uint value,
         address payable recipient
-    ) public payable restricted {
-        Request storage request = requests[currentRequestIndex];
+    ) public restricted {
+        Request storage request = requests.push();
 
         request.description = description;
         request.value = value;
         request.recipient = recipient;
         request.complete = false;
         request.approvalCount = 0;
-
-        currentRequestIndex++;
     }
 }

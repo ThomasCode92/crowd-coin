63d2f70f83533fe94227f6e3979688dd151e0b40
ThomasCode92 <thomascode92@hotmail.com>
2023-10-30 18:01:09 +0100

    Fetching Deployed Campaigns

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,18 @@
+import { useEffect } from 'react';
+import campaignFactory from '@/utils/factory';
+
 export default function Home() {
+  useEffect(() => {
+    const fetchCampaigns = async () => {
+      const campaigns = await campaignFactory.methods
+        .getDeployedCampaigns()
+        .call();
+
+      console.log(campaigns);
+    };
+
+    fetchCampaigns();
+  }, []);
+
   return <h1>Welcome to Crowd Coin</h1>;
 }

655e7a0d620c396274b277bc99feeccc5948f1ee
ThomasCode92 <thomascode92@hotmail.com>
2023-11-01 11:25:19 +0100

    Rendering Card Groups

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import { Fragment } from 'react';
+import { Card } from 'semantic-ui-react';
+
 import campaignFactory from '@/utils/factory';
 
 export default function Home({ campaigns }) {
-  console.log(campaigns);
+  const campaignItems = campaigns.map(address => ({
+    header: address,
+    description: <a>View Campaign</a>,
+    fluid: true,
+  }));
 
-  return <h1>Welcome to Crowd Coin</h1>;
+  return (
+    <Fragment>
+      <h1>Welcome to Crowd Coin</h1>
+      <Card.Group items={campaignItems} />
+    </Fragment>
+  );
 }
 
 export async function getStaticProps() {

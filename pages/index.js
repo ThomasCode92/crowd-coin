031765aca7ba1dcd2b544b20056044f6462fd94c
ThomasCode92 <thomascode92@hotmail.com>
2023-11-01 09:09:11 +0100

    getStaticProps Function

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-import { useEffect } from 'react';
 import campaignFactory from '@/utils/factory';
 
-export default function Home() {
-  useEffect(() => {
-    const fetchCampaigns = async () => {
-      const campaigns = await campaignFactory.methods
-        .getDeployedCampaigns()
-        .call();
-
-      console.log(campaigns);
-    };
-
-    fetchCampaigns();
-  }, []);
+export default function Home({ campaigns }) {
+  console.log(campaigns);
 
   return <h1>Welcome to Crowd Coin</h1>;
+}
+
+export async function getStaticProps() {
+  const campaigns = await campaignFactory.methods.getDeployedCampaigns().call();
+  return { props: { campaigns } };
 }

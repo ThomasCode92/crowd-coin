e7d8e214d177671e60504eca6fa6bd652fcbdd66
ThomasCode92 <thomascode92@hotmail.com>
2023-11-02 18:43:15 +0100

    Adding a Button

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Button, Card } from 'semantic-ui-react';
 
 import campaignFactory from '@/utils/factory';
 
@@ -14,6 +14,7 @@ export default function Home({ campaigns }) {
     <Fragment>
       <h1>Welcome to Crowd Coin</h1>
       <Card.Group items={campaignItems} />
+      <Button content="Create Campaign" icon="add circle" primary />
     </Fragment>
   );
 }

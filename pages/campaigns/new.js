7282670d42287e4796b699c6f5ab4dc815ff392b
ThomasCode92 <thomascode92@hotmail.com>
2023-11-07 20:06:14 +0100

    Form Creation

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,3 +1,17 @@
+import { Fragment } from 'react';
+import { Button, Form } from 'semantic-ui-react';
+
 export default function NewCampaign() {
-  return <h1>New Campaign!</h1>;
+  return (
+    <Fragment>
+      <h1>Create a Campaign</h1>
+      <Form>
+        <Form.Field>
+          <label>Minimum Contribution</label>
+          <input />
+        </Form.Field>
+        <Button primary>Create</Button>
+      </Form>
+    </Fragment>
+  );
 }

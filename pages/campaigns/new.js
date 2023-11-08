bee42cd1ce659e99c138407f898369bb69a7ecda
ThomasCode92 <thomascode92@hotmail.com>
2023-11-08 09:50:23 +0100

    Form Error Handling

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,28 +1,33 @@
 import { Fragment, useState } from 'react';
-import { Button, Form, Input } from 'semantic-ui-react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
 
 import campaignFactory from '@/utils/factory';
 import web3 from '@/utils/web3';
 
 export default function NewCampaign() {
   const [minimumContribution, setMinimumContribution] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const submitHandler = async event => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    const { createCampaign } = campaignFactory.methods;
+    try {
+      const accounts = await web3.eth.getAccounts();
+      const { createCampaign } = campaignFactory.methods;
 
-    await createCampaign(minimumContribution).send({
-      from: accounts[0],
-      data: createCampaign(minimumContribution).encodeABI(),
-    });
+      await createCampaign(minimumContribution).send({
+        from: accounts[0],
+        data: createCampaign(minimumContribution).encodeABI(),
+      });
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
   };
 
   return (
     <Fragment>
       <h1>Create a Campaign</h1>
-      <Form onSubmit={submitHandler}>
+      <Form error={Boolean(errorMessage)} onSubmit={submitHandler}>
         <Form.Field>
           <label>Minimum Contribution</label>
           <Input
@@ -32,6 +37,7 @@ export default function NewCampaign() {
             onChange={event => setMinimumContribution(event.target.value)}
           />
         </Form.Field>
+        <Message error header="Something went wrong!" content={errorMessage} />
         <Button primary content="Create" />
       </Form>
     </Fragment>

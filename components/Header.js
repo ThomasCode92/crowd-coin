bb91e97d46da1705c6cee400f78a7391be1665fd
ThomasCode92 <thomascode92@hotmail.com>
2023-11-13 17:12:20 +0100

    Header Navigation

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,17 @@
+import { useRouter } from 'next/router';
 import { Icon, Menu } from 'semantic-ui-react';
+
 import styles from './Header.module.css';
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header className={styles.header}>
       <Menu>
-        <Menu.Item>CrowdCoin</Menu.Item>
+        <Menu.Item onClick={() => router.push('/')}>CrowdCoin</Menu.Item>
         <Menu.Menu position="right">
           <Menu.Item>Campaigns</Menu.Item>
-          <Menu.Item>
+          <Menu.Item onClick={() => router.push('/campaigns/new')}>
             <Icon name="plus" />
           </Menu.Item>
         </Menu.Menu>

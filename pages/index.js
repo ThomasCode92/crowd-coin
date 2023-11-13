6e1c445bed2305d1f04b0555331e39fae26e1e17
ThomasCode92 <thomascode92@hotmail.com>
2023-11-13 17:16:16 +0100

    Routing to Campaigns

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,26 @@ import { Fragment } from 'react';
 import { Button, Card } from 'semantic-ui-react';
 
 import campaignFactory from '@/utils/factory';
+import Link from 'next/link';
 
 export default function Home({ campaigns }) {
   const campaignItems = campaigns.map(address => ({
     header: address,
-    description: <a>View Campaign</a>,
+    description: <Link href={'campaigns/' + address}>View Campaign</Link>,
     fluid: true,
   }));
 
   return (
     <Fragment>
       <h1>Open Campaigns</h1>
-      <Button
-        content="Create Campaign"
-        icon="add circle"
-        primary
-        floated="right"
-      />
+      <Link href="/campaigns/new">
+        <Button
+          content="Create Campaign"
+          icon="add circle"
+          primary
+          floated="right"
+        />
+      </Link>
       <Card.Group items={campaignItems} />
     </Fragment>
   );

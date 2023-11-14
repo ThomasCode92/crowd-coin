ed294b08330cebd273ce6696b23eb5b7ad6ac907
ThomasCode92 <thomascode92@hotmail.com>
2023-11-14 20:32:48 +0100

    Refactor Campaign Details

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,55 +1,19 @@
 import { Fragment } from 'react';
 import { Card } from 'semantic-ui-react';
 
-import web3 from '@/utils/web3';
-import { getCampaign, getCampaignSummary } from '@/utils/campaign';
+import {
+  getCampaign,
+  getCampaignDetails,
+  getCampaignSummary,
+} from '@/utils/campaign';
 
 export default function showCampaign(props) {
-  const {
-    balance,
-    manager,
-    minimumContribution,
-    requestsCount,
-    approversCount,
-  } = props.summary;
-
-  const items = [
-    {
-      header: manager,
-      meta: 'Address of Manager',
-      description:
-        'The manager created this campaign and can create requests to withdraw money',
-      style: { overflowWrap: 'break-word' },
-    },
-    {
-      header: minimumContribution,
-      meta: 'Minimum Contribution (wei)',
-      description:
-        'You must contribute at least this much wei to become an approver',
-    },
-    {
-      header: requestsCount,
-      meta: 'Number of Requests',
-      description:
-        'A request tries to withdraw money from the contract. Requests must be approved by approvers',
-    },
-    {
-      header: approversCount,
-      meta: 'Number of Approvers',
-      description: 'Number of people who have already donated to this campaign',
-    },
-    {
-      header: web3.utils.fromWei(balance, 'ether'),
-      meta: 'Campaign Balance (ether)',
-      description:
-        'The balance is how much money this campaign has left to spend.',
-    },
-  ];
+  const campaignDetails = getCampaignDetails(props.summary);
 
   return (
     <Fragment>
       <h1>Campaign details</h1>
-      <Card.Group items={items} />
+      <Card.Group items={campaignDetails} />
     </Fragment>
   );
 }
--- utils/campaign.js
+++ utils/campaign.js
@@ -14,3 +14,46 @@ export const getCampaignSummary = data => {
     manager: data[4],
   };
 };
+
+export const getCampaignDetails = summary => {
+  const {
+    balance,
+    manager,
+    minimumContribution,
+    requestsCount,
+    approversCount,
+  } = summary;
+
+  return [
+    {
+      header: manager,
+      meta: 'Address of Manager',
+      description:
+        'The manager created this campaign and can create requests to withdraw money',
+      style: { overflowWrap: 'break-word' },
+    },
+    {
+      header: minimumContribution,
+      meta: 'Minimum Contribution (wei)',
+      description:
+        'You must contribute at least this much wei to become an approver',
+    },
+    {
+      header: requestsCount,
+      meta: 'Number of Requests',
+      description:
+        'A request tries to withdraw money from the contract. Requests must be approved by approvers',
+    },
+    {
+      header: approversCount,
+      meta: 'Number of Approvers',
+      description: 'Number of people who have already donated to this campaign',
+    },
+    {
+      header: web3.utils.fromWei(balance, 'ether'),
+      meta: 'Campaign Balance (ether)',
+      description:
+        'The balance is how much money this campaign has left to spend.',
+    },
+  ];
+};

81091c546c0645ea04e7309740a54e14f3423e39
ThomasCode92 <thomascode92@hotmail.com>
2023-11-14 20:25:36 +0100

    Custom Card Groups

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,7 +1,27 @@
+import { Fragment } from 'react';
+import { Card } from 'semantic-ui-react';
+
 import { getCampaign, getCampaignSummary } from '@/utils/campaign';
 
-export default function showCampaign() {
-  return <div>Show Campaign</div>;
+export default function showCampaign(props) {
+  const { manager } = props.summary;
+
+  const items = [
+    {
+      header: manager,
+      meta: 'Address of Manager',
+      description:
+        'The manager created this campaign and can create requests to withdraw money',
+      style: { overflowWrap: 'break-word' },
+    },
+  ];
+
+  return (
+    <Fragment>
+      <h1>Campaign details</h1>
+      <Card.Group items={items} />
+    </Fragment>
+  );
 }
 
 export async function getServerSideProps(context) {

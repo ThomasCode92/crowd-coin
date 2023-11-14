da740e28da131e254656c7a6bb2adb579f2f8b7b
ThomasCode92 <thomascode92@hotmail.com>
2023-11-14 18:29:10 +0100

    showCampaign's getServerSideProps

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,3 +1,8 @@
 export default function showCampaign() {
   return <div>Show Campaign</div>;
 }
+
+export async function getServerSideProps(context) {
+  console.log(context.query.address);
+  return { props: {} };
+}

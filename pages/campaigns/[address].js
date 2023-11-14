04eb20b26e4bf1819a7abb5a80b1b09463108911
ThomasCode92 <thomascode92@hotmail.com>
2023-11-14 20:26:29 +0100

    One Card per Property

--- pages/campaigns/[address].js
+++ pages/campaigns/[address].js
@@ -1,10 +1,17 @@
 import { Fragment } from 'react';
 import { Card } from 'semantic-ui-react';
 
+import web3 from '@/utils/web3';
 import { getCampaign, getCampaignSummary } from '@/utils/campaign';
 
 export default function showCampaign(props) {
-  const { manager } = props.summary;
+  const {
+    balance,
+    manager,
+    minimumContribution,
+    requestsCount,
+    approversCount,
+  } = props.summary;
 
   const items = [
     {
@@ -13,6 +20,29 @@ export default function showCampaign(props) {
       description:
         'The manager created this campaign and can create requests to withdraw money',
       style: { overflowWrap: 'break-word' },
+    },
+    {
+      header: minimumContribution,
+      meta: 'Minimum Contribution (wei)',
+      description:
+        'You must contribute at least this much wei to become an approver',
+    },
+    {
+      header: requestsCount,
+      meta: 'Number of Requests',
+      description:
+        'A request tries to withdraw money from the contract. Requests must be approved by approvers',
+    },
+    {
+      header: approversCount,
+      meta: 'Number of Approvers',
+      description: 'Number of people who have already donated to this campaign',
+    },
+    {
+      header: web3.utils.fromWei(balance, 'ether'),
+      meta: 'Campaign Balance (ether)',
+      description:
+        'The balance is how much money this campaign has left to spend.',
     },
   ];
 

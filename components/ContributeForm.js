3c0e6187deee8ed77f2cf1446349e3bbc7e36e19
ThomasCode92 <thomascode92@hotmail.com>
2023-11-15 08:01:16 +0100

    Making a Contribution

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -2,14 +2,26 @@ import { useState } from 'react';
 import { Button, Form, Input } from 'semantic-ui-react';
 
 import { getCampaign } from '@/utils/campaign';
+import web3 from '@/utils/web3';
 
 export default function ContributeForm({ address }) {
   const [contribution, setContribution] = useState('');
 
-  const submitHandler = event => {
+  const submitHandler = async event => {
     event.preventDefault();
 
-    const campaign = getCampaign(address);
+    try {
+      const accounts = await web3.eth.getAccounts();
+
+      const campaign = getCampaign(address);
+      const { contribute } = campaign.methods;
+
+      await contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(contribution, 'ether'),
+        data: contribute().encodeABI(),
+      });
+    } catch (error) {}
   };
 
   return (

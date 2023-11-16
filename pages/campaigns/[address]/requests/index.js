901a7ced197e17d4a19e82f48c5de28df4df2a27
ThomasCode92 <thomascode92@hotmail.com>
2023-11-16 08:07:01 +0100

    More Routing!

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,3 +1,18 @@
-export default function CampaignRequests() {
-  return <h1>Request List</h1>;
+import Link from 'next/link';
+import { Fragment } from 'react';
+import { Button } from 'semantic-ui-react';
+
+export default function CampaignRequests({ address }) {
+  return (
+    <Fragment>
+      <h1>Request List</h1>
+      <Link href={`/campaigns/${address}/requests/new`}>
+        <Button primary>Add Request</Button>
+      </Link>
+    </Fragment>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return { props: { address: context.query.address } };
 }
---
+++ pages/campaigns/[address]/requests/new.js
@@ -0,0 +1,9 @@
+import { Fragment } from 'react';
+
+export default function NewRequest() {
+  return (
+    <Fragment>
+      <h1>Create a Request</h1>
+    </Fragment>
+  );
+}

ee4f2993a8367d7325d631daf8d5d88c57046502
ThomasCode92 <thomascode92@hotmail.com>
2023-11-16 18:30:42 +0100

    Request Creation Form

--- pages/campaigns/[address]/requests/new.js
+++ pages/campaigns/[address]/requests/new.js
@@ -1,9 +1,42 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
+import { Button, Form, Input } from 'semantic-ui-react';
 
-export default function NewRequest() {
+export default function NewRequest({ address }) {
+  const [description, setDescription] = useState('');
+  const [value, setValue] = useState('');
+  const [recipient, setRecipient] = useState('');
+
   return (
     <Fragment>
       <h1>Create a Request</h1>
+      <Form>
+        <Form.Field>
+          <label>Description</label>
+          <Input
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Value in Ether</label>
+          <Input
+            value={value}
+            onChange={event => setValue(event.target.value)}
+          />
+        </Form.Field>
+        <Form.Field>
+          <label>Recipient</label>
+          <Input
+            value={recipient}
+            onChange={event => setRecipient(event.target.value)}
+          />
+        </Form.Field>
+        <Button primary content="Create" />
+      </Form>
     </Fragment>
   );
+}
+
+export async function getServerSideProps(context) {
+  return { props: { address: context.query.address } };
 }

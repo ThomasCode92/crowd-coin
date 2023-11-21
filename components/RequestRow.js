153835b03f90fe7768610af8b0a5ad2429f6c745
ThomasCode92 <thomascode92@hotmail.com>
2023-11-21 21:56:52 +0100

    Row Status Styling

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -2,34 +2,54 @@ import { Button, Table } from 'semantic-ui-react';
 
 import web3 from '@/utils/web3';
 import { getCampaign } from '@/utils/campaign';
+import { useRouter } from 'next/router';
 
 export default function RequestRow({ request, approversCount, address }) {
+  const router = useRouter();
+
+  const readyToFinalize = request.approvalCount > approversCount / 2;
+
   const approveHandler = async () => {
-    const accounts = await web3.eth.getAccounts();
+    try {
+      const accounts = await web3.eth.getAccounts();
 
-    const campaign = getCampaign(address);
-    const { approveRequest } = campaign.methods;
+      const campaign = getCampaign(address);
+      const { approveRequest } = campaign.methods;
 
-    await approveRequest(request.idx).send({
-      from: accounts[0],
-      data: approveRequest(request.idx).encodeABI(),
-    });
+      await approveRequest(request.idx).send({
+        from: accounts[0],
+        data: approveRequest(request.idx).encodeABI(),
+      });
+
+      router.reload();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   const finalizeHandler = async () => {
-    const accounts = await web3.eth.getAccounts();
+    try {
+      const accounts = await web3.eth.getAccounts();
 
-    const campaign = getCampaign(address);
-    const { finalizeRequest } = campaign.methods;
+      const campaign = getCampaign(address);
+      const { finalizeRequest } = campaign.methods;
 
-    await finalizeRequest(request.idx).send({
-      from: accounts[0],
-      data: finalizeRequest(request.idx).encodeABI(),
-    });
+      await finalizeRequest(request.idx).send({
+        from: accounts[0],
+        data: finalizeRequest(request.idx).encodeABI(),
+      });
+
+      router.reload();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
-    <Table.Row>
+    <Table.Row
+      disabled={request.complete}
+      positive={!request.complete && readyToFinalize}
+    >
       <Table.Cell>{request.idx}</Table.Cell>
       <Table.Cell>{request.description}</Table.Cell>
       <Table.Cell>{web3.utils.fromWei(request.value, 'ether')}</Table.Cell>
@@ -38,12 +58,22 @@ export default function RequestRow({ request, approversCount, address }) {
         {request.approvalCount}/{approversCount}
       </Table.Cell>
       <Table.Cell>
-        <Button basic color="green" onClick={approveHandler}>
+        <Button
+          basic
+          color="green"
+          onClick={approveHandler}
+          disabled={request.complete}
+        >
           Approve
         </Button>
       </Table.Cell>
       <Table.Cell>
-        <Button basic color="teal" onClick={finalizeHandler}>
+        <Button
+          basic
+          color="teal"
+          onClick={finalizeHandler}
+          disabled={request.complete}
+        >
           Finalize
         </Button>
       </Table.Cell>
